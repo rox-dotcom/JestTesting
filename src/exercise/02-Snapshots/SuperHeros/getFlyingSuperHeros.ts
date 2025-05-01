1bdce952c5d6f925d980524ae20ac2788d53e98f
rox-dotcom <roxarandagr@gmail.com>
2025-04-30 20:26:06 -0600

    Branch bank Activity added

--- src/exercise/02-Snapshots/SuperHeros/getFlyingSuperHeros.ts
+++
@@ -1,8 +0,0 @@
-interface ISuperHero {
-  name: string;
-  power: string[];
-}
-
-export function getFlyingSuperHeros(superHeros: ISuperHero[]): ISuperHero[] {
-  return superHeros.filter((superHero) => superHero.power.includes("Fly"));
-}
--- src/exercise/07-FormTesting/loginBuilder.ts
+++
@@ -1,9 +0,0 @@
-// src/exercise/Exercise04to08/07-FormTesting/loginBuilder.ts
-
-import { build, perBuild } from '@jackfranklin/test-data-bot';
-import { faker } from '@faker-js/faker';
-
-export const buildLoginForm = build({
-  username: perBuild(() => faker.internet.userName()),
-  password: perBuild(() => faker.internet.password()),
-});
